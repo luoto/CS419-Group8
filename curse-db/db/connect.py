ebbfe4a80907bfe62db2f012a9f8dd7ace14d52f
Tony Luo <luoto@onid.oregonstate.edu>
2015-07-23 21:51:54 -0400

    Reactor into classes

--- curse-db/db/connect.py
+++ curse-db/db/connect.py
@@ -1,17 +1,42 @@
 import psycopg2
 
-def read(filename):
-    inFile = open('../settings/' + filename).read()
-    return inFile
 
-def connect():
-    connection_string = read('config.txt')
-    try:
-        conn = psycopg2.connect(connection_string)
-        curr = conn.cursor()
-        return curr
-    except:
-        print "Unable to connect to database"
+class DB:
+    """Generic class that is meant to be used as an interface for subclasses.
+
+    This class is not meant to be used directly. It provides a template for
+    subclasses that deal specifically with vendors.
+    """
+
+    def __init__(self):
+        pass
+
+    def _read(self, fname):
+        inFile = open('../settings/' + fname).read()
+        return inFile
+
+    def connect(self):
+        pass
+
+
+class Psql(DB):
+    """This class handles all connections and queries related to PostgreSQL.
+
+    """
+    def __init__(self):
+        DB.__init__(self)
+
+    def connect(self):
+        connection_string = self._read('config.txt')
+        print connection_string
+        try:
+            conn = psycopg2.connect(connection_string)
+            curr = conn.cursor()
+            return curr
+        except psycopg2.Error, e:
+            print e
+
 
 if __name__ == '__main__':
-    connect()
+    db = Psql()
+    db.connect()
