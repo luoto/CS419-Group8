a2f36a3c73688419e56d136f66b2cd0bf71fb9f4
Tony Luo <luoto@onid.oregonstate.edu>
2015-07-25 15:02:53 -0400

    Fix import

--- curse_db/tests/utils_tests.py
+++ curse_db/tests/utils_tests.py
@@ -1,10 +1,12 @@
 import unittest
-import curse_db.utils as utils
+import sys
+sys.path.append('../utils')
+import utils
 
 class TestUtils(unittest.TestCase):
 
     def test_arrayify(self):
-        self.assertEqual(utils.arrayify('Hello World'), ['Hello', 'World'])
+        self.assertEqual(utils.arrayify('Hello World'), ['Hello', 'Worlds'])
 
 if __name__ == '__main__':
     unittest.main()
