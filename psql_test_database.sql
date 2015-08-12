3f6bdff84b5573fdf284518ab0b4afa31f09b216
Tony Luo <luo.tony37@gmail.com>
2015-08-12 10:33:39 -0700

    Fix relations error

--- psql_test_database.sql
+++ psql_test_database.sql
@@ -7,16 +7,16 @@ CREATE DATABASE curses;
 
 \c curses
 
+CREATE TABLE studios (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(25) NOT NULL
+);
+
 CREATE TABLE movies (
   id SERIAL PRIMARY KEY,
   title VARCHAR(25) NOT NULL,
   genre VARCHAR(25),
   studio_id INT references studios(id)
-);
-
-CREATE TABLE studios (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(25) NOT NULL
 );
 
 INSERT INTO studios (name) VALUES
